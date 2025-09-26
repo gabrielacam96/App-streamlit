3d5dc472436bd06a96be9c97d7ab96fb76b73fdb
gabriela.campoverde <gabriela.campoverde@altia.es>
2025-09-26 14:24:40 +0200

    primer commit

---
+++ mi_app_streamlit/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from modules.utils import cargar_datos
+import numpy as np
+st.title('Demo de estructura')
+df = cargar_datos('data/ventas.csv')
+st.dataframe(df)
+
+
+valor = st.slider('Selecciona un valor', 0, 100, 50)  # Slider interactivo
+resultado = valor ** 2  # Cálculo reactivo
+st.write(f'El cuadrado de {valor} es {resultado}')  # Muestra el resultado
+
+def incrementar():
+    st.session_state.contador += 1  # Aumenta el contador en 1
+
+if 'contador' not in st.session_state:
+    st.session_state.contador = 0  # Inicializa el contador
+
+st.button('Sumar', on_click=incrementar)  # Botón con callback
+st.write(f'Contador: {st.session_state.contador}')  # Muestra el valor
